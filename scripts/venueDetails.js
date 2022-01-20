d0e3bfa96693aebb888053141bf0ea4d777ba375
Bishal Samanta <web.dev.bishalsamanta@gmail.com>
2022-01-20 20:26:00 +0530

    Done with append

--- scripts/venueDetails.js
+++ scripts/venueDetails.js
@@ -29,14 +29,53 @@ function first(){
 let data = JSON.parse(localStorage.getItem("venueDetails"))
 console.log(data)
 
-//displayUi(data);
+displayUi(data);
 
 function displayUi(data){
+    document.querySelector("#container").innerHTML = "";
+    document.querySelector("#selected-location").textContent = data[0].location
     data.map((el)=>{
        // console.log(el)
 
         //Destructuring ob objects
-        let { name, } = el;
+        let { name, imgUrl,location ,rating , filter_by} = el;
+       
+        let iconLink = "https://playo-website.gumlet.net/icons/sh.jpeg?auto=compress,format&q=90";
+
+
+        //Function for votes
+        let votesCount = Math.floor(100 + Math.random() * 900);
+
+        //Create Html Template
+        let mainDiv = document.createElement("div");
+        let mainImg = document.createElement("img");
+        let nameDiv = document.createElement("div");
+        let locationDiv = document.createElement("div");
+        let playDiv = document.createElement("div");
+        let lastDiv = document.createElement("div");
+        let BookableDiv = document.createElement("div");
+        let CovidDiv = document.createElement("div");
+        let safeImg = document.createElement("img");
+        let safeDiv = document.createElement("div");
+        let ratingDiv = document.createElement("div");
+        let votesDiv = document.createElement("div");
+
+        //Set values for all div+
+        mainImg.src = imgUrl;
+        nameDiv.textContent = name;
+        locationDiv.textContent = location;
+        playDiv.textContent = filter_by[0];
+        BookableDiv.textContent = "BOOKABLE";
+        safeImg.src = iconLink;
+        ratingDiv.textContent = rating;
+        votesDiv.textContent = `${votesCount} votes`
+
+        //Append Properly
+        CovidDiv.append(safeImg, safeDiv);
+        lastDiv.append(BookableDiv, CovidDiv);
+        mainDiv.append(mainImg, nameDiv, locationDiv, playDiv, lastDiv, ratingDiv, votesDiv);
+        document.querySelector("#container").append(mainDiv);
+        
 
 
 
