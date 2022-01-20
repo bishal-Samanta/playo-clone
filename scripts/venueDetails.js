7b67aeee919c152ef849cdc3012dafdfac7bb2a8
Bishal Samanta <web.dev.bishalsamanta@gmail.com>
2022-01-20 19:57:03 +0530

    Appent html template for book venue

--- scripts/venueDetails.js
+++ scripts/venueDetails.js
@@ -1,9 +1,47 @@
 
 
+
+
+
 //Filter Products
-document.querySelector(".filter").addEventListener("click", ()=>{
 
-    
+//On clicking appear an reappear of div
+document.querySelector(".filterbox").style.visibility = "hidden";
+document.querySelector(".filter").addEventListener("click", first);
+
+function first(){
+    document.querySelector(".filterbox").style.visibility = "visible";
+    this.removeEventListener("click", first);
+
+    document.querySelector(".filter").addEventListener("click", ()=>{
+        document.querySelector(".filterbox").style.visibility = "hidden";
+        
+        document.querySelector(".filter").addEventListener("click", first);
+
+    })
+}
+
+//On clicking appear an reappear of div
 
 
-}) 
+
+//Append with the data
+let data = JSON.parse(localStorage.getItem("venueDetails"))
+console.log(data)
+
+//displayUi(data);
+
+function displayUi(data){
+    data.map((el)=>{
+       // console.log(el)
+
+        //Destructuring ob objects
+        let { name, } = el;
+
+
+
+    })
+}
+
+
+
