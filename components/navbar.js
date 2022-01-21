4ba3af8926b714f19f75bd9c3f81c4c3ff793711
Shivangi Mall <smdp365@gmail.com>
2022-01-21 18:09:57 +0530

    connected pages with index
    connected book venues, blog, footer to index

--- components/navbar.js
+++ components/navbar.js
@@ -5,7 +5,7 @@ function navbar(){
                 </a>
 
                 <ul id="nav_options">
-                    <li><a href="/"><span class="link-btn">Book Venues</span></a></li>
+                    <li><a href="bookVenues.html"><span class="link-btn">Book Venues</span></a></li>
                     <li><a href="/"><span class="link-btn">Activites</span></a></li>
                     <li><a href="blog.html"><span class="link-btn">Blog</span></a></li>
                 </ul>
