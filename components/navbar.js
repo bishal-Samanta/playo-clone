4f64c67e9a6efc26db396c1811aea2f6eba87e4b
Bishal Samanta <web.dev.bishalsamanta@gmail.com>
2022-01-21 22:48:19 +0530

    activities.html done 1

--- components/navbar.js
+++ components/navbar.js
@@ -6,7 +6,7 @@ function navbar(){
 
                 <ul id="nav_options">
                     <li><a href="venues.html"><span class="link-btn">Book Venues</span></a></li>
-                    <li><a href="/"><span class="link-btn">Activites</span></a></li>
+                    <li><a href="activities.html"><span class="link-btn">Activites</span></a></li>
                     <li><a href="blog.html"><span class="link-btn">Blog</span></a></li>
                 </ul>
             </div>`;
